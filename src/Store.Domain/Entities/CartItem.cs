bc55790567cd9afb278b1be219d1dca97ae55718
Alexander <alexrgs@gmail.com>
2019-06-24 21:56:04 -0600

    Rearrange properties order

--- src/Store.Domain/Entities/CartItem.cs
+++ src/Store.Domain/Entities/CartItem.cs
@@ -3,10 +3,10 @@
     public class CartItem
     {
         public int CartItemId { get; set; }
-        public int CartId { get; set; }
-        public int ProductId { get; set; }
         public short Quantity { get; set; }
+        public int CartId { get; set; }
         public Cart Cart { get; set; }
+        public int ProductId { get; set; }
         public Product Product { get; set; }
     }
 }

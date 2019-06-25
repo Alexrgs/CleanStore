833b0e4b2541b37655ccf567a4da0f2576fbbbf7
Alexander <alexrgs@gmail.com>
2019-06-24 21:55:06 -0600

    Added missing navegation

--- src/Store.Domain/Entities/Product.cs
+++ src/Store.Domain/Entities/Product.cs
@@ -5,12 +5,18 @@ using System.Text;
 namespace Store.Domain.Entities
 {
     public class Product
-    {   
+    {
+        public Product()
+        {
+            CartItems = new HashSet<CartItem>();
+        }
+
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
         public string ProductImgURL { get; set; }
         public decimal? UnitPrice { get; set; }
         public short? UnitsInStock { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
     }
 }

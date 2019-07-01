1ab9e2001d3110c3f09fbcb91bf65a1856f0c45d
Alexander <alexrgs@gmail.com>
2019-07-01 11:15:56 -0600

    Modified product Item to use cards

--- src/Store.WebUI/ClientApp/src/components/ProductItem.js
+++ src/Store.WebUI/ClientApp/src/components/ProductItem.js
@@ -1,22 +1,21 @@
 ﻿import React from 'react';
-import { Well, Col, Row, Button } from 'react-bootstrap';
-import Img from 'react-image'
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap';
 
 class ProductItem extends React.Component {
    
     render() {
         return (
-            <Well>
-                <Row>
-                    <Col xs={12} className='productItem'>
-                        <Img src={[this.props.product.productImgURL,'./img/No_image_3x4.svg']} alt={this.props.product.productName} height={100} />                         
-                        <h4>{this.props.product.productName}</h4>
-                        <p>{this.props.product.productDescription}</p>
-                        <p>Price: $ {this.props.product.unitPrice}</p>
-                        <Button onClick={() => this.props.handleOnAdd(this.props.product)} bsStyle='primary'>ADD</Button>
-                    </Col>
-                </Row>
-            </Well>
+            <Card >
+                <CardImg top  src={this.props.product.productImgURL} alt={this.props.product.productName} />
+                <CardBody>
+                    <CardTitle>{this.props.product.productName}</CardTitle>
+                    <CardSubtitle>$ {this.props.product.unitPrice}</CardSubtitle>
+                    <CardText>
+                        {this.props.product.productDescription}
+                    </CardText>
+                    <Button onClick={() => this.props.handleOnAdd(this.props.product)} variant="primary">Buy</Button>
+                </CardBody>
+            </Card>
         );
     }
 }
